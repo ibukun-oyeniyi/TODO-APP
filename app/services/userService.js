e4bf785744677940d0fb37af6cb70db1a6e00168
Ibukun Oyeniyi <ibukun.oyeniyi@babbangona.com>
2023-03-25 19:02:57 +0100

    servieces restructuring

---
+++ app/services/userService.js
@@ -0,0 +1,16 @@
+const userDao = require("../dao/user.Dao")
+
+const findUser =  (email,done)=>{
+    //call the userdao finduser method
+    
+    userDao.findUser(email,done)
+}
+
+const registerUser= (userData,done)=>{
+    //call the userdao finduser method
+    userDao.registerUser(userData,done)
+}
+
+module.exports={
+    findUser,registerUser
+}
