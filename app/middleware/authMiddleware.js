4b844ddcc950edcd19f8f5e167b06371ec8d6978
Ibukun Oyeniyi <ibukun.oyeniyi@babbangona.com>
2023-03-26 22:57:25 +0100

    added verify token function

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -1,15 +1,36 @@
+const jwt = require("jsonwebtoken")
+const { createError } = require("./error")
+
+//Verifying the token generated in the login Page
+const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (authHeader) {
+        const token = authHeader.split(" ")[1]
+        jwt.verify(
+            token,
+            process.env.AUTH_SECRET || "secret",
+            (err, user) => {
+                if (err) return next(createError(403, "Your Token is Invalid"))
+                req.user = user
+                next();
+            }
+        );
+    } else {
+        res.status(401).json("You are not Authorized yet")
+    }
+}
+
+//Verifying the User after verifying the token
+const verifyUser = async (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === parseInt(req.params.userId)) {
+            next();
+        } else {
+            res.status(401).json("You are not Authorized to do this")
+        }
+    })
+}
 
 
-//import jsonwebtoken and config
 
-//This function verifyToken will verify the token coming from headers 
-const verifyToken = (req, res, next) => {
-    // Getting the authorization header
-    const token = req.headers["authorization"];
-  
-   
-  //Synchronously verify given token using a secret or a public key to get a decoded token 
-   
-    //return next
-    return next();
-  };
+module.exports = { verifyToken, verifyUser }
